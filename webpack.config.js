c03621e0fdb42ee93d8b6f3232ff3c26853d1204
Your Name <bill9414@hotmail.com>
2020-12-01 16:12:36 +0100

    material-ui

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'webpack.bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js||jsx)$/,
+            exclude: /node-modules/,
+            use: {
+                loader: 'babel-loader',
+            }  
+          }
+        ]
+    },
+    {
+        test: /\.html$/i,
+        loader: 'html-loader',
     }
 };
